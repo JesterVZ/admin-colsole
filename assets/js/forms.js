49d62e3f6b0831a9e76e40e6496ca1c3fb0a3112
VLadimir Zhulin <vladimir.a.zhulin@gmail.com>
2019-10-10 00:12:42 +0300

    start commit

---
+++ assets/js/forms.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.create-post' + '-btn').on('click', function() {
+        showOverlay();
+        showCreatePostForm('-create-post');
+    });
+    $('.settings label').on('click', function() {
+        showOverlay();
+        showSettingsForm('-settings');
+    });
+    $('.fa-times').on('click', function() {
+        $('.popup').css('display', 'none');
+        $('.overlay').css('display', 'none');
+
+    });
+
+
+    function showCreatePostForm(e) {
+        $('.popup' + e).css('display', 'block');
+    }
+
+    function showSettingsForm(e) {
+        $('.popup' + e).css('display', 'block');
+    }
+
+    function showOverlay() {
+        $('.overlay').css('display', 'block');
+    }
+
+});
